a343cf8d578159f2248f29c8658943af8deb7f71
Yonatan <yonatan.ramirez557@gmail.com>
2018-06-21 14:42:38 -0700

    practice 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-}
+
+  // tslint:disable-next-line:no-inferrable-types
+  bools: boolean = false;
+
+  nums: Array<number> = [1, 2.5, 5];
+
+  strs: Array<string> = ['hi', 'there'];
+
+  test = ({
+    id: 1,
+    name: 'test'
+  });
+
+  user = ({
+    id: 2,
+    name: 'user'
+  });
+
+  objs = [this.test, this.user ];
+
+  types = [this.bools, this.nums, this.strs];
+
+  getTypeof(type) {
+    return typeof type;
+  }
+ }
